cd28e80ba9d74f7893798c1058c951e3e9e8b51c
Passle <pascalschilp@gmail.com>
2019-07-29 18:52:26 +0200

    add webpack title

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -107,7 +107,7 @@ export default () => {
         html`
           <dialog open>
             <h1><i>Perflink</i></h1>
-            <h2 class="lite">Lite</h2>
+            <h2 class="lite">Webpack</h2>
             <h3>Live Javascript Benchmarking</h3>
             <p>
               Write scripts and race them. See results graphed out as you type.
